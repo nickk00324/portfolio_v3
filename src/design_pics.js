75f4ce83a66d55364abc569de736d34cffe71f67
nickk <nickkochornswasdi@gmail.com>
2019-12-20 20:55:53 -0800

    fixed bug with file paths :P

--- src/design_pics.js
+++ src/design_pics.js
@@ -1,19 +1,19 @@
 export const designPics = {
          "future ghosts": [
-           "../assets/imgs/design/future_ghosts/future_ghosts_web.jpg"
+           "assets/imgs/design/future_ghosts/future_ghosts_web.jpg"
          ],
          "why does everything have to be so annoying and stupid?!": [
-           "../assets/imgs/design/why_does/why_does.png"
+           "assets/imgs/design/why_does/why_does.png"
          ],
          "haunts": [
-           "../assets/imgs/design/haunts/haunts_flyer_square.jpg",
-           "../assets/imgs/design/haunts/haunts_postcard.png",
-           "../assets/imgs/design/haunts/haunts_web.png"
+           "assets/imgs/design/haunts/haunts_flyer_square.jpg",
+           "assets/imgs/design/haunts/haunts_postcard.png",
+           "assets/imgs/design/haunts/haunts_web.png"
          ],
          "nick kochornswasdi gallery": [
-           "../assets/imgs/design/nkg/huchu.jpg",
-           "../assets/imgs/design/nkg/01_philippFarra_IS_web.jpg",
-           "../assets/imgs/design/nkg/02_diegoBarrientos_PBD.jpg",
-           "../assets/imgs/design/nkg/nkg_web.png"
+           "assets/imgs/design/nkg/huchu.jpg",
+           "assets/imgs/design/nkg/01_philippFarra_IS_web.jpg",
+           "assets/imgs/design/nkg/02_diegoBarrientos_PBD.jpg",
+           "assets/imgs/design/nkg/nkg_web.png"
          ]
        };
--- src/project_pics.js
+++ src/project_pics.js
@@ -1,34 +1,34 @@
 export const projectPics = {
          "not real twitter": [
            "https://fast-atoll-83889.herokuapp.com/#/",
-           "../assets/imgs/projects/not_real_twitter/not_real_twitter.png"
+           "assets/imgs/projects/not_real_twitter/not_real_twitter.png"
          ],
          "haunts website": [
            "http://haunts.xyz",
-           "../assets/imgs/design/haunts/haunts_web.png"
+           "assets/imgs/design/haunts/haunts_web.png"
          ],
          demongraphics: [
            "https://nickk00324.github.io/demongraphics",
-           "../assets/imgs/projects/demongraphics/demongraphics.png"
+           "assets/imgs/projects/demongraphics/demongraphics.png"
          ],
          "nick kochornswasdi gallery website": [
            "https://nickkochornswasdigallery.com",
-           "../assets/imgs/design/nkg/nkg_web.png"
+           "assets/imgs/design/nkg/nkg_web.png"
          ],
          "we're gonna have fucking fun!": [
            "http://www.lulu.com/shop/nick-kochornswasdi/were-gonna-have-fucking-fun/paperback/product-24134341.html",
-           "../assets/imgs/projects/fucking_fun/fucking_fun_cover.png"
+           "assets/imgs/projects/fucking_fun/fucking_fun_cover.png"
          ],
          "thank you (book)": [
            "http://www.lulu.com/shop/nicolas-kochornswasdi/thank-you/paperback/product-23981524.html?ppn=1",
-           "../assets/imgs/projects/thank_you/TY_cover_copy.png"
+           "assets/imgs/projects/thank_you/TY_cover_copy.png"
          ],
          "things are looking up": [
            "https://night-hugs.itch.io/things-are-looking-up",
-           "../assets/imgs/projects/looking_up/talu.png"
+           "assets/imgs/projects/looking_up/talu.png"
          ],
          "please come over": [
            "https://night-hugs.itch.io/please-come-over",
-           "../assets/imgs/projects/please_come_over/please_come_over.png"
+           "assets/imgs/projects/please_come_over/please_come_over.png"
          ]
        };
