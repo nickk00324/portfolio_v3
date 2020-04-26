7139409c88d0cd5502b9e89170047b11871344a8
nickk <nickkochornswasdi@gmail.com>
2020-04-25 17:56:46 -0700

    added lumpy, updated colors, added new nkg

--- src/project_pics.js
+++ src/project_pics.js
@@ -1,38 +1,42 @@
 export const projectPics = {
-         "not real twitter": [
-           "https://fast-atoll-83889.herokuapp.com/#/",
-           "assets/imgs/projects/not_real_twitter/not_real_twitter.png"
-         ],
-         "haunts website": [
-           "http://haunts.xyz",
-           "assets/imgs/design/haunts/haunts_web.png"
-         ],
-         demongraphics: [
-           "https://nickk00324.github.io/demongraphics",
-           "assets/imgs/projects/demongraphics/demongraphics.png"
-         ],
-         "nick kochornswasdi gallery website": [
-           "https://nickkochornswasdigallery.com",
-           "assets/imgs/design/nkg/nkg_web.png"
-         ],
-         "we're gonna have fucking fun!": [
-           "http://www.lulu.com/shop/nick-kochornswasdi/were-gonna-have-fucking-fun/paperback/product-24134341.html",
-           "assets/imgs/projects/fucking_fun/fucking_fun_cover.png"
-         ],
-         "thank you (book)": [
-           "http://www.lulu.com/shop/nicolas-kochornswasdi/thank-you/paperback/product-23981524.html?ppn=1",
-           "assets/imgs/projects/thank_you/TY_cover_copy.png"
-         ],
-         "things are looking up": [
-           "https://night-hugs.itch.io/things-are-looking-up",
-           "assets/imgs/projects/looking_up/talu.png"
-         ],
-         "please come over": [
-           "https://night-hugs.itch.io/please-come-over",
-           "assets/imgs/projects/please_come_over/please_come_over.png"
-         ],
-         "doth": [
-           "https://nickk00324.github.io/doth",
-           "assets/imgs/projects/doth/doth.png"
-         ]
-       };
+  "not real twitter": [
+    "https://fast-atoll-83889.herokuapp.com/#/",
+    "assets/imgs/projects/not_real_twitter/not_real_twitter.png",
+  ],
+  "haunts website": [
+    "http://haunts.xyz",
+    "assets/imgs/design/haunts/haunts_web.png",
+  ],
+  demongraphics: [
+    "https://nickk00324.github.io/demongraphics",
+    "assets/imgs/projects/demongraphics/demongraphics.png",
+  ],
+  "nick kochornswasdi gallery website": [
+    "https://nickkochornswasdigallery.com",
+    "assets/imgs/design/nkg/nkg_web.png",
+  ],
+  "we're gonna have fucking fun!": [
+    "http://www.lulu.com/shop/nick-kochornswasdi/were-gonna-have-fucking-fun/paperback/product-24134341.html",
+    "assets/imgs/projects/fucking_fun/fucking_fun_cover.png",
+  ],
+  "thank you (book)": [
+    "http://www.lulu.com/shop/nicolas-kochornswasdi/thank-you/paperback/product-23981524.html?ppn=1",
+    "assets/imgs/projects/thank_you/TY_cover_copy.png",
+  ],
+  "things are looking up": [
+    "https://night-hugs.itch.io/things-are-looking-up",
+    "assets/imgs/projects/looking_up/talu.png",
+  ],
+  "please come over": [
+    "https://night-hugs.itch.io/please-come-over",
+    "assets/imgs/projects/please_come_over/please_come_over.png",
+  ],
+  doth: [
+    "https://nickk00324.github.io/doth",
+    "assets/imgs/projects/doth/doth.png",
+  ],
+  "the revolting lumpen! at the airport": [
+    "https://lumpyairport.com",
+    "assets/imgs/projects/lumpen/lumpy.png",
+  ],
+};
